51a89d3e7a96941dbef179935c3a6ff2f94e4873
Huseyn Novruzov <huseyn.novruzov.123@gmail.com>
2022-07-04 12:57:56 +0300

    remove import django_heroku statement in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from decouple import config
 import os
-import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -121,17 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 SUMMERNOTE_THEME = 'bs4'
-django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
