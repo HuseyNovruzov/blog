43090992042fca132a1af0e184c0d64343f8b0a6
Huseyn Novruzov <huseyn.novruzov.123@gmail.com>
2022-07-07 15:51:31 +0300

    AWS s3 is configured

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'django_summernote',
     'mptt',
+    'storages',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -37,6 +38,22 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#AWS
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None 
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'static'
+
+DEFAULT_FILESTORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 AUTH_USER_MODEL = 'base.CustomUser'
 
@@ -120,12 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')  
 MEDIA_URL = '/images/'
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')  
-
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
